8f749ec00f348d11d9fdc2396fac16e25e1bfc52
Oleksandr Karpinskyi <redfield242@gmail.com>
2025-03-29 15:26:41 +0200

    feat. add Footer component

--- lab1/App.js
+++ lab1/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Footer} from "./Components/Footer";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start worasdaking on your app!</Text>
       <StatusBar style='auto' />
+      <Footer></Footer>
     </View>
+
   );
 }
 
